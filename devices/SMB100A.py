55d79a52cc4016f9a88dab0e83255b236c2a6ddc
steelelab <steelelab@TUD07152.dastud>
2016-08-05 20:55:19 +0200

    more broken instrument links fixed

--- devices/SMB100A.py
+++ devices/SMB100A.py
@@ -2,12 +2,13 @@
 
 import visa
 import numpy as np
-from instrument import instrument
+from stlab.devices.instrument import instrument
 
 def numtostr(mystr):
     return '%12.8e' % mystr
 
 
 class SMB100A_SG(instrument):
-    def __init__(self,addr='TCPIP::192.168.1.151::INSTR',reset=True):
+    def __init__(self,addr='TCPIP::192.168.1.43::INSTR',reset=True):
         super(SMB100A_SG, self).__init__(addr,reset)
+        self.id()
