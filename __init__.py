8979ee74153f78420e72405eb29c3c8e31af9c95
steelelab <steelelab@TUD07152.dastud>
2016-08-26 14:26:46 +0200

    Added shortcuts to utils in __init__.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+#Loads utils with shorter names
+from stlab.utils.newfile import newfile
+import stlab.utils.metagen as metagen
+from stlab.utils.S11fit import fit as S11fit
+import stlab.utils.readdata as readdata
