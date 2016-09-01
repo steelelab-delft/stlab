091d5a5ecdbe9f92b7dc80ff97bcbf285c870f19
yausern <yausern@gmail.com>
2016-09-01 15:27:13 +0200

    Added comment in newfile.py

--- utils/newfile.py
+++ utils/newfile.py
@@ -4,6 +4,7 @@ import shutil
 
 # Creates new measurement folder using prefix + datetime + idstring.
 # If colnames (array of column names) is included, the title line is written
+# Also copies main script file to measurement folder
 
 def newfile(prefix,idstring,colnames=None):
 
