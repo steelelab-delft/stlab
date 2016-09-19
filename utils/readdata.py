04508ba1996350149818561fa49e6e04e43116ba
yausern <yausern@gmail.com>
2016-09-19 13:23:55 +0200

    Changed opening files using "with" instead of just open

--- utils/readdata.py
+++ utils/readdata.py
@@ -7,53 +7,51 @@ import numpy as np
 from collections import OrderedDict
 
 def readdat(filename):
-    f = open(filename,'r')
-    variables = {}
-    ivar = 0
-    mylists = {}
-    col = []
-    currentvarname =""
+    with open(filename,'r') as f:
+        variables = {}
+        ivar = 0
+        mylists = {}
+        col = []
+        currentvarname =""
 
-    line = f.readline()
-    line = line.strip("\n").strip("# ")
-    print(line)
-    names = line.split(', ')
-    print(names)
+        line = f.readline()
+        line = line.strip("\n").strip("# ")
+        print(line)
+        names = line.split(', ')
+        print(names)
 
-    block =[]
-    arrayofdicts = []
-    for point in f:
-        if point == '\n':
-            block = np.asarray(block)
-            block = block.T
-            newdict=OrderedDict()
-            for name,dat in zip(names,block):
-                newdict[name] = dat
-            arrayofdicts.append(newdict)
-            block = []
-            continue
-        point = [ float(x) for x in point.strip('\n').split(', ')]
-        block.append(point)
-
-    return arrayofdicts
+        block =[]
+        arrayofdicts = []
+        for point in f:
+            if point == '\n':
+                block = np.asarray(block)
+                block = block.T
+                newdict=OrderedDict()
+                for name,dat in zip(names,block):
+                    newdict[name] = dat
+                arrayofdicts.append(newdict)
+                block = []
+                continue
+            point = [ float(x) for x in point.strip('\n').split(', ')]
+            block.append(point)
+        return arrayofdicts
 
 def reads2p(filename):
-	f = open(filename,'r')
-	names = ['Frequency (Hz)', 'S11re ()', 'S11im ()', 'S21re ()', 'S21im ()', 'S12re ()', 'S12im ()', 'S22re ()', 'S22im ()']
+    with open(filename,'r') as f:
+        names = ['Frequency (Hz)', 'S11re ()', 'S11im ()', 'S21re ()', 'S21im ()', 'S12re ()', 'S12im ()', 'S22re ()', 'S22im ()']
 
-	measurement = []
-	for line in f:
-		if line.startswith('!') or line.startswith('#'):
-			continue
-		line = [ float(x) for x in line.strip('\n').split()]
-		measurement.append(line)
-	measurement = np.asarray(measurement)
-	measurement = measurement.T
-	mydict=OrderedDict()
-	for name,data in zip(names,measurement):
-		mydict[name]=data
-	f.close()
-	return mydict
+        measurement = []
+        for line in f:
+            if line.startswith('!') or line.startswith('#'):
+                continue
+            line = [ float(x) for x in line.strip('\n').split()]
+            measurement.append(line)
+        measurement = np.asarray(measurement)
+        measurement = measurement.T
+        mydict=OrderedDict()
+        for name,data in zip(names,measurement):
+            mydict[name]=data
+        return mydict
 
 # Imports a QUCS formatted data file.  The data is returned as a dict 
 # containing np.array's for each variable with the QUCS variable name
@@ -62,63 +60,35 @@ def reads2p(filename):
 pi = np.pi
 
 def readQUCS(filename):
-    f = open(filename,'r')
-    variables = {}
-    ivar = 0
-    mylists = {}
-    col = []
-    currentvarname =""
+    with open(filename,'r') as f:
+        variables = {}
+        ivar = 0
+        mylists = {}
+        col = []
+        currentvarname =""
 
-    for line in f:
-        varfound = line.find('<')
-        varendfound = line.find('/')
-        if varendfound != -1:
-            mylists[currentvarname] = np.array(col)
-            col = []
-        elif varfound != -1:
-            line = line.strip("\n").strip(">").strip("<")
-            words = line.split()
-            vartype = words[0]
-            varname = words[1]
-            print(vartype, varname)
-            if vartype == 'Qucs':
-                continue
-            elif vartype == 'indep' or vartype=='dep':
-                currentvarname = varname
-        elif varfound == -1:
-            if 'j' not in line:
-                col.append(float(line))
-            else:
-                ij = line.find('j')
-                x = float(line[0:ij-1])
-                y = float(line[ij-1:].replace("j",""))
-                col.append( complex(x,y) )
-    return mylists
-
-
-'''
-        varfound = line.find('<')
-        varendfound = line.find('/')
-        if varendfound != -1:
-            mylists[currentvarname] = np.array(col)
-            col = []
-        elif varfound != -1:
-            line = line.strip("\n").strip(">").strip("<")
-            words = line.split()
-            vartype = words[0]
-            varname = words[1]
-            print vartype, varname
-        if vartype == 'Qucs':
-        continue
-    elif vartype == 'indep' or vartype=='dep':
-            currentvarname = varname
-    elif varfound == -1:
-    if 'j' not in line:
-        col.append(float(line))
-    else:
-        ij = line.find('j')
-        x = float(line[0:ij-1])
-        y = float(line[ij-1:].replace("j",""))
-        col.append( complex(x,y) )
-    return mylists
-'''
+        for line in f:
+            varfound = line.find('<')
+            varendfound = line.find('/')
+            if varendfound != -1:
+                mylists[currentvarname] = np.array(col)
+                col = []
+            elif varfound != -1:
+                line = line.strip("\n").strip(">").strip("<")
+                words = line.split()
+                vartype = words[0]
+                varname = words[1]
+                print(vartype, varname)
+                if vartype == 'Qucs':
+                    continue
+                elif vartype == 'indep' or vartype=='dep':
+                    currentvarname = varname
+            elif varfound == -1:
+                if 'j' not in line:
+                    col.append(float(line))
+                else:
+                    ij = line.find('j')
+                    x = float(line[0:ij-1])
+                    y = float(line[ij-1:].replace("j",""))
+                    col.append( complex(x,y) )
+        return mylists
