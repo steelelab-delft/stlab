8a4ea2bf3af2de190bcd876a7c7e0ac313712b54
yausern <yausern@gmail.com>
2016-10-06 14:53:05 +0200

    Refined IVVI driver

--- devices/IVVI.py
+++ devices/IVVI.py
@@ -7,14 +7,33 @@ class pol(Enum):
     positive = 'POS'
     negative = 'NEG'
   
-
+# IVVI DACs basic driver.  The system only reads and writes short byte arrays.
+# DOES NOT INHERIT FROM THE "INSTRUMENT" CLASS
 class IVVI_DAC:
+    # Constructor for IVVI Dac class.  Address is ASRLCOM1 if on COM1 serial port
+    # The Serial parameters for this device are:
+    # Baud = 115200, data bits = 8, parity = ODD, stop bit = 1
     def __init__(self,addr='ASRLCOM1',ndacs=8,polarity=('BIP','BIP'),verb=True,reset=True):
-        self.dev = instrument.global_rs.open_resource(addr,baud_rate=115200,data_bits=8,parity=cts.Parity.odd,stop_bits=cts.StopBits.one,read_termination=None,write_termination=None)
+        # Open serial resource or other
+        if "ASRL" in addr:         
+            self.dev = instrument.global_rs.open_resource(addr,baud_rate=115200,data_bits=8,parity=cts.Parity.odd,stop_bits=cts.StopBits.one)
+        else:
+            self.dev = instrument.global_rs.open_resource(addr)
+        self.dev.read_termination=None
+        self.dev.write_termination=None
         self.ndacs = ndacs
-        self.polarity = [pol(x) for x in polarity]
         self.verb = verb
+        self.SetPolarity(polarity)
+        return
+    def SetPolarity(self,polarity):
+        self.polarity = [pol(x) for x in polarity]
         self.make_polmatrix()
+        if self.verb:        
+            print('Polarity settings')
+            for x,y in zip(self.polarity,range(4)):
+                print('DACs'+'%d-%d: ' % ((y+1)*4-3,(y+1)*4) + x.value)
+            print('polmatrix = ', self.polmatrix)
+        return
     def make_polmatrix(self):
         polmatrix = []
         print(self.polarity)
@@ -26,7 +45,7 @@ class IVVI_DAC:
             elif mypol is pol.negative:
                 polmatrix = polmatrix + [4000. for x in range(4)]
         self.polmatrix = tuple(polmatrix)
-        print(self.polmatrix)
+        return
     def numbers_to_mvoltages(self, numbers):
         '''
         Converts a list of bytes to a list containing
@@ -45,6 +64,13 @@ class IVVI_DAC:
             (dataH, dataL) (int, int) : The high and low value byte equivalent
         '''
         bytevalue = int(round(mvoltage/4000.0*65535))
+        if bytevalue > 65535 or bytevalue < 0:
+            print("DAC Warning: mvoltage value out of range: ", mvoltage, bytevalue)
+            print("Setting to limit")
+            if bytevalue < 0:
+                bytevalue = 0
+            if bytevalue > 65535:
+                bytevalue = 65535
         dataH = int(bytevalue/256)
         dataL = bytevalue - dataH*256
         return (dataH, dataL)
@@ -52,18 +78,27 @@ class IVVI_DAC:
         (DataH, DataL) = self.mvoltage_to_bytes(mvoltage+self.polmatrix[dac-1])
         #print(DataH, DataL)
         message = (7, 0, 2, 1, dac, DataH, DataL)
+        if self.verb:
+            print("DAC input: ", message)
         message = bytearray(message)
         self.dev.write_raw(message)
         reply,status = self.dev.visalib.read(self.dev.session,2)
-        for x in reply:
-            print(x,type(x))
+        if verb:
+            for x in reply:
+                print("DAC reply: ", reply)
     def ReadDACs(self):
         message = (4, 0, self.ndacs*2+2, 2)
+        if self.verb:
+            print("DAC input: ", message)
         message = bytearray(message)
         self.dev.write_raw(message)
         reply,status = self.dev.visalib.read(self.dev.session,2)
+        if self.verb:
+            print('DAC reply: ', reply)
         nbytes = reply[0]
         reply,status = self.dev.visalib.read(self.dev.session,nbytes-2)
         reply = tuple([x for x in reply])
+        if self.verb:
+            print('DAC reply: ', reply)
         reply = self.numbers_to_mvoltages(reply)
         return reply
