663aa0c34c80aa8b9208c478b63464bffb133f82
Local Administrator <Local Administrator>
2016-11-23 19:34:06 +0100

    Changes to TritonDaemon to log in per day folders

--- devices/TritonWrapper/TritonDaemon/TritonDaemon.py
+++ devices/TritonWrapper/TritonDaemon/TritonDaemon.py
@@ -5,10 +5,12 @@ import time
 from logger import logger
 import socket
 from stlab.utils.MySocket import MySocket
+import sys
+import datetime
 
 
 def command_handler(qin):
-    mytriton =  Triton(verb=False)
+    mytriton =  Triton()
     while True:
         nextcomm = qin.get()
         if nextcomm == 0:
@@ -21,7 +23,23 @@ def command_handler(qin):
         qout.put(ret)
 
 print("StLab Temperature server for Triton.  Initializing...")
-        
+
+
+'''
+if len(sys.argv) >= 2:
+    filename = sys.argv[1]
+    ff = open(filename,'a')
+    ff.write('\n')
+else:
+    t0 = datetime.datetime.now()
+    filename = 'log_' + t0.strftime('%y_%m_%d__%H_%M_%S') + '.dat'
+    varline = ['Time (s)'] + ['PT2 Head (K)','PT2 Plate (K)', 'Still Plate (K)','Cold Plate (K)','MC Cernox (K)','PT1 Head (K)','PT1 Plate (K)','MC Plate (K)'] + ['P%d (mbar)' % i for i in range(1,7)]
+    #varline = ['Time (s)'] + ['T%d (K)' % i for i in range(1,10)] + ['P%d (mbar)' % i for i in range(1,7)]
+    print(varline)
+    ff = open(filename,'w')
+    ff.write('#' + ', '.join(varline)+'\n')
+'''
+    
 commandq = Queue(maxsize=0)
    
 myhandler = Thread(target=command_handler, args=(commandq,))
@@ -44,30 +62,27 @@ print("Ready.  Listening on port %d and address %s" % (port,addr))
 
 def RunCommand(sock,resultq):
     ss = MySocket(sock)
-    
-    word = s.myreceive()
+    word = ss.myreceive()
     word = word.decode('utf_8')
-    
     commandq.put( (resultq, Triton.query, (word,)) )
     xx = resultq.get()
     resultq.task_done()
-    
     ss.mysend(xx.encode('utf_8'))
     ss.sock.close()
-    
     return word
     
 resultq = Queue(maxsize=0)
     
-try:
-    while True:
+while True:
+    clientsocket = None
+    try:
         # accept connections from outside
         (clientsocket, address) = serversocket.accept()
         RunCommand(clientsocket,resultq)
-        
-except KeyboardInterrupt:
-    print('Shutting down temperature server')
-    serversocket.close()
+    except KeyboardInterrupt:
+        print('Shutting down temperature server')
+        serversocket.close()
+        break
 
 commandq.put(0)
 loggerthread.join()
--- devices/TritonWrapper/TritonDaemon/logger.py
+++ devices/TritonWrapper/TritonDaemon/logger.py
@@ -5,13 +5,44 @@ import time
 import datetime
 from queue import Queue
 from stlab.devices.triton import Triton
+import os
 
-def logger(commandq,ff):
+varline = ['Time (s)'] + ['PT2 Head (K)','PT2 Plate (K)', 'Still Plate (K)','Cold Plate (K)','MC Cernox (K)','PT1 Head (K)','PT1 Plate (K)','MC Plate (K)'] + ['P%d (mbar)' % i for i in range(1,7)]
+
+def logger(commandq):
+    
     resultq = Queue(maxsize=0)
+    today = datetime.date.today()
+    foldername = './%02d-%02d-%02d/' % (today.day,today.month,today.year)
+    filename = foldername + 'log_%02d-%02d-%02d.dat' % (today.day,today.month,today.year)
+    if not os.path.exists(foldername):
+        os.makedirs(foldername)
+    print('Measurement Folder: ', foldername)
+    if os.path.isfile(filename):
+        ff = open(filename,'a')
+    else:
+        ff = open(filename,'w')
+        ff.write('#' + ', '.join(varline)+'\n')
+      
+    
     
     # Main logging loop.  Exit with keyboard interrupt (CTRL + C)
     try:
         while True:
+            newday = datetime.date.today()
+            if today != newday:
+                    ff.close()
+                    today = newday
+                    foldername = './%02d-%02d-%02d/' % (today.day,today.month,today.year)
+                    filename = foldername + 'log_%02d-%02d-%02d.dat' % (today.day,today.month,today.year)
+                    if not os.path.exists(foldername):
+                        os.makedirs(foldername)
+                    print('New day - Measurement Folder: ', foldername)
+                    if os.path.isfile(filename):
+                        ff = open(filename,'a')
+                    else:
+                        ff = open(filename,'w')
+                        ff.write('#' + ', '.join(varline)+'\n')
             line = []
             for i in range(1,10):
                 commandq.put( (resultq, Triton.gettemperature, (i,)) )
@@ -38,5 +69,4 @@ def logger(commandq,ff):
             time.sleep(30)
     except KeyboardInterrupt:
         ff.close()
-        print('Goodbye!')
-    return
+    print('Goodbye!')
