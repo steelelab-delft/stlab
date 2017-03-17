ee10454c072a8e55dc537d26f88dabf252fd01b3
Local Administrator <Local Administrator>
2017-03-17 14:25:54 +0100

    Fixed bug in newly added pna _test

--- examples/pna_test.py
+++ examples/pna_test.py
@@ -6,17 +6,17 @@ from stlab.devices.PNAN5221A import PNAN5221A as pnaclass
 
 from matplotlib import pyplot as plt
 
-pna = pnaclass(addr='TCPIP::192.168.1.123::INSTR',reset=False,verb=True)
+pna = pnaclass(addr='TCPIP::192.168.1.105::INSTR',reset=False,verb=True)
 
 pna.SetRange(4e9,8e9)
-pna.SetIFWB(1000.)
+pna.SetIFBW(1000.)
 pna.SetPower(-25.6)
 pna.SetPoints(2001)
 
 data = pna.MeasureScreen()
 keys = list(data.keys())
 print(keys)
-for i in range(0,(len(keys)-1)/3):
+for i in range(0,int((len(keys)-1)/3)):
     x = data[keys[0]]
     y = data[keys[3*(i+1)]]
     plt.plot(x,y)
@@ -30,7 +30,7 @@ pna.SetPower(-5.)
 data = pna.MeasureScreen()
 keys = list(data.keys())
 print(keys)
-for i in range(0,(len(keys)-1)/3):
+for i in range(0,int((len(keys)-1)/3)):
     x = data[keys[0]]
     y = data[keys[3*(i+1)]]
     plt.plot(x,y)
