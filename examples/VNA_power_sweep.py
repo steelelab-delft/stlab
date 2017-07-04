bd3b9f31d54881fd258a10b91b0b373ee7b1e05f
Local Administrator <Local Administrator>
2017-07-04 18:54:03 +0200

    Finally fixed VNA_power_sweep example

--- examples/VNA_power_sweep.py
+++ examples/VNA_power_sweep.py
@@ -5,7 +5,7 @@ import numpy as np
 prefix = 'M2' #prefix for measurement folder name.  Can be anything or empty
 idstring = 'Dev1_powersweep' #Additional info included in measurement folder name.  Can be anything or empty
 
-pna = pnaclass(addr='TCPIP::192.168.1.151::INSTR',reset=True,verb=True) #Initialize device communication and reset
+pna = pnaclass(addr='TCPIP::192.168.1.151::INSTR',reset=False,verb=True) #Initialize device communication and reset
 
 pna.SetRange(4e9,8e9) #Set frequency range in Hz
 pna.SetIFBW(300.) #Set IF bandwidth in Hz
@@ -18,14 +18,8 @@ powers = np.linspace(powstart,powstop,steps) #generate power sweep steps
 
 for i,rfpower in enumerate(powers):
     pna.SetPower(rfpower) #set pna power
-    data = pna.Measure2ports() #Trigger 2 port measurement and retrieve data in Re,Im format.  Returns OrderedDict
+    data = pna.MeasureScreen() #Trigger 2 port measurement and retrieve data in Re,Im format.  Returns OrderedDict
     #Add Columns for S parameters in dB
-    S11dB = 20.*np.log10( [ np.sqrt(np.power(a,2.)+np.power(b,2.)) for a,b in zip(data['S11re ()'],data['S11im ()']) ] )
-    S21dB = 20.*np.log10( [ np.sqrt(np.power(a,2.)+np.power(b,2.)) for a,b in zip(data['S21re ()'],data['S21im ()']) ] )
-    npoints = len(data['Frequency (Hz)'])
-    data['Power (dBm)'] = np.full(npoints,rfpower)
-    data['S11dB (dB)'] = S11dB
-    data['S21dB (dB)'] = S21dB
     if i==0: #if on first measurement, create new measurement file and folder using titles extracted from measurement
         myfile = stlab.newfile(prefix,idstring,data.keys())
     stlab.savedict(myfile, data) #Save measured data to file.  Written as a block for spyview.
