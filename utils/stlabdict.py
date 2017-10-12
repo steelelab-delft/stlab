ea8595c1e94e073e48a7081014907982e8194cec
yausern <yausern@gmail.com>
2017-10-12 18:21:57 +0200

    Added functions to stlabdict

--- utils/stlabdict.py
+++ utils/stlabdict.py
@@ -10,3 +10,34 @@ class stlabdict(OrderedDict):
         n = len(x)
         self[colname] = np.full(n,colval)
         return
+    def line(self,nn):
+        ret = stlabdict()
+        for key in self.keys():
+            ret[key] = self[key][nn]
+        return ret
+    def __getitem__(self, key):
+        if key in self.keys():
+            return super(stlabdict, self).__getitem__(key)
+        elif isinstance( key, int ) and key >= 0:
+            return self[list(self.keys())[key]]
+        else:
+            raise KeyError
+    def ncol(self):
+        return len(self.keys())
+    def nline(self):
+        a = len(self[0])
+        for key in self.keys():
+            if len(self[key]) is not a:
+                print('Columns with different length!!?')
+        return a
+    def matrix(self):
+        mat = []
+        for key in self.keys():
+            col = []
+            for x in self[key]:
+                col.append(x)
+            mat.append(col)
+        mat = np.transpose(mat)
+        return mat
+            
+            
