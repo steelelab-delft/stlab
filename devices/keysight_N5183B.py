61ca9a943637149cb49c56e960b454dd99c34de6
Local Administrator <Local Administrator>
2018-02-01 12:20:34 +0100

    Added keysight N5183B

---
+++ devices/keysight_N5183B.py
@@ -0,0 +1,36 @@
+# Driver for R&S SMB100A signal generator.  Inherits from instrument class.
+
+import visa
+import numpy as np
+from stlab.devices.instrument import instrument
+
+def numtostr(mystr):
+    return '%12.8e' % mystr
+
+
+class keysight_N5183B(instrument):
+    def __init__(self,addr='TCPIP::192.168.1.237::INSTR',reset=True,verb=True):
+        super(keysight_N5183B, self).__init__(addr,reset,verb)
+        self.id()
+    def setCWfrequency(self,frec):
+        mystr =  numtostr(frec)
+        mystr = 'FREQ:CW ' + mystr
+        self.write(mystr)
+    def getCWfrequency(self):
+        mystr = 'FREQ:CW?'
+        pp = self.query(mystr)
+        pp = float(pp)
+        return pp
+    def setCWpower(self,x):
+        mystr =  numtostr(x)
+        mystr = 'SOUR:POW ' + mystr
+        self.write(mystr)
+    def getCWpower(self):
+        mystr = 'SOUR:POW?'
+        pp = self.query(mystr)
+        pp = float(pp)
+        return pp
+    def RFon(self):
+        self.write('OUTP ON')
+    def RFoff(self):
+        self.write('OUTP OFF')
