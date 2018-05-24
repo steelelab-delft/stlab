814f5a3fc6828f64efde3559bc531fc9fac2e8b0
yausern <yausern@gmail.com>
2018-05-24 10:27:40 +0200

    minor

--- devices/BFDaemon/BFDaemon.py
+++ devices/BFDaemon/BFDaemon.py
@@ -46,10 +46,10 @@ else:
 
 def yes_or_no(question):
     while "the answer is invalid":
-        reply = str(raw_input(question+' (y/n): ')).lower().strip()
-        if reply[0] == 'y':
+        reply = str(input(question+' (y/n): ')).lower().strip()
+        if reply[:1] == 'y':
             return True
-        if reply[0] == 'n':
+        if reply[:1] == 'n':
             return False
 
 #define input queue for the command handler    
