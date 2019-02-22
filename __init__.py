0e95867407dde042a27ec7e335c9bc162de53a8d
feschmidt <felix.e.schmidt@gmail.com>
2019-02-22 13:50:42 +0100

    Update __init__.py
    Modify __init__.py so stlab can be used for analysis only

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-#Loads utils with shorter names
+# Loads utils with shorter names
 from stlab.utils.newfile import newfile #Callable as stlab.newfile(...)
 import stlab.utils.metagen as metagen #Callable as stlab.metagen.fromlimits(...)
 import stlab.utils.readdata as readdata #Callable as stlab.readdata.readQUCS(...)
@@ -14,12 +14,19 @@ from stlab.utils.writematrix import writeframearray as saveframearray
 from stlab.utils.stlabdict import stlabdict
 from stlab.utils.stlabdict import stlabmtx
 from stlab.utils.stlabdict import framearr_to_mtx
-from stlab.utils.S11fit import fit as S11fit #Callable as stlab.S11fit(...)
-from stlab.utils.S11fit import S11full as S11func #Callable as stlab.S11fit(...)
-from stlab.utils.S11fit import backmodel as S11back #Callable as stlab.S11fit(...)
-from stlab.utils.S11fit import S11theo as S11theo #Callable as stlab.S11fit(...)
-#from stlab.devices.base_instrument import SaveInstrumentMetadata #Callable as stlab.SaveInstrumentMetadata(...)
-from stlab.devices.autodetect_instrument import autodetect_instrument as adi #Callable as stlab.devices.adi(...)
+try: # some users might not need S11fit
+    from stlab.utils.S11fit import fit as S11fit #Callable as stlab.S11fit(...)
+    from stlab.utils.S11fit import S11full as S11func #Callable as stlab.S11fit(...)
+    from stlab.utils.S11fit import backmodel as S11back #Callable as stlab.S11fit(...)
+    from stlab.utils.S11fit import S11theo as S11theo #Callable as stlab.S11fit(...)
+except:
+    print('Warning: Missing dependencies for S11fit! Fitting routines not imported.')
+try: # some users might not need to do measurements
+    #from stlab.devices.base_instrument import SaveInstrumentMetadata #Callable as stlab.SaveInstrumentMetadata(...)
+    from stlab.devices.autodetect_instrument import autodetect_instrument as adi #Callable as stlab.devices.adi(...)
+except:
+    print('Warning: Missing dependencies for autodetect_instrument! Autodetect routines not imported.')
+
 from stlab.utils.autoplotter import autoplot #Call as stlab.autoplot(...)
 
 '''
