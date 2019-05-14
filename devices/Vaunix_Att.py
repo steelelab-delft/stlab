21b2812833ec50117c3e99a5dd48d8705ebfc5da
feschmidt <felix.e.schmidt@gmail.com>
2019-05-14 19:02:52 +0200

    vaunix att done

--- devices/Vaunix_Att.py
+++ devices/Vaunix_Att.py
@@ -43,87 +43,42 @@ class Vaunix_Att():
             print('status:', status)
 
         # weird conversion factors
-        self.ffreq = 10
-        self.fpow = 1 / 4
+        self.fatt = 1 / 4
 
         # limits
-        # self.max_power = self.lib.fnLDA_GetMaxPwr(self.device_id) * self.fpow
-        # self.min_power = self.lib.fnLDA_GetMinPwr(self.device_id) * self.fpow
-        # self.max_freq = self.lib.fnLDA_GetMaxFreq(self.device_id) * self.ffreq
-        # self.min_freq = self.lib.fnLDA_GetMinFreq(self.device_id) * self.ffreq
+        self.max_att = self.lib.fnLDA_GetMaxAttenuation(
+            self.device_id) * self.fatt
+        self.min_att = self.lib.fnLDA_GetMinAttenuation(
+            self.device_id) * self.fatt
 
         return
 
     def close(self):
         self.lib.fnLDA_CloseDevice(self.device_id)
 
-    def CheckLimits(self, value, quantity):
-        if (quantity == 'frequency') or (quantity == 'freq'):
-            if value < self.min_freq:
-                print('Warning: Frequency out of range. Set to min = {} GHz'.
-                      format(self.min_freq / 1e9))
-                self.SetFrequency(self.min_freq)
-            elif value > self.max_freq:
-                print('Warning: Frequency out of range. Set to max = {} GHz'.
-                      format(self.max_freq / 1e9))
-                self.SetFrequency(self.max_freq)
-        elif (quantity == 'power') or (quantity == 'pow'):
-            if value < self.min_power:
-                print(
-                    'Warning: Power out of range. Set to min = {} dBm'.format(
-                        self.min_power))
-                self.SetPower(self.min_power)
-            elif value > self.max_power:
-                print(
-                    'Warning: Power out of range. Set to max = {} dBm'.format(
-                        self.max_power))
-                self.SetPower(self.max_power)
-        else:
-            raise KeyError('quantity must be either freq[ency] or pow[er]')
+    def CheckLimits(self, value):
+        if value < self.min_att:
+            print(
+                'Warning: Attenuation out of range. Set to min = {} dB'.format(
+                    self.min_att))
+            self.SetAttenuation(self.min_att)
+        elif value > self.mamax_attx_power:
+            print(
+                'Warning: Attenuation out of range. Set to max = {} dB'.format(
+                    self.max_att))
+            self.SetAttenuation(self.max_att)
 
     def GetDeviceID(self):
         return self.device_id
 
-    def SetRFOn(self):
-        self.lib.fnLDA_SetRFOn(self.device_id, 1)
+    def SetAttenuation(self, att):
+        self.CheckLimits(att')
+        a0 = int(att / self.fatt)
+        self.lib.fnLDA_SetAttenuation(self.device_id, a0)
 
-    def SetRFOff(self):
-        self.lib.fnLDA_SetRFOn(self.device_id, 0)
-
-    def SetFrequency(self, freq):
-        self.CheckLimits(freq, 'freq')
-        f0 = int(freq / self.ffreq)
-        self.lib.fnLDA_SetFrequency(self.device_id, f0)
-
-    def GetFrequency(self):
-        f0 = float(self.lib.fnLDA_GetFrequency(self.device_id) * self.ffreq)
-        return f0
-
-    def SetPower(self, pow):
-        self.CheckLimits(pow, 'pow')
-        p0 = int(pow / self.fpow)
-        self.lib.fnLDA_SetPowerLevel(self.device_id, p0)
-
-    def GetPower(self):
-        p0 = float(self.lib.fnLDA_GetPowerLevel(self.device_id) * self.fpow)
-        return p0
-
-    def SetReference(self, ref):
-        if ref == 'INT':
-            self.lib.fnLDA_SetUseInternalRef(self.device_id, 1)
-        elif ref == 'EXT':
-            self.lib.fnLDA_SetUseInternalRef(self.device_id, 0)
-        else:
-            raise ValueError('Unknown reference')
-
-    def GetReference(self):
-        msg = self.lib.fnLDA_GetUseInternalRef(self.device_id)
-        if msg == 0:
-            return 'EXT'
-        elif msg == 1:
-            return 'INT'
-        else:
-            raise ValueError('Unknown reference')
+    def GetAttenuation(self):
+        a0 = float(self.lib.fnLDA_GetAttenuation(self.device_id) * self.fatt)
+        return a0
 
     def GetMetadataString(self):
         # Should return a string of metadata adequate to write to a file
