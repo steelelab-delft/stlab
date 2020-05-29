5c57660f16314a0c6bfa5ad1b8c1d2b135ed3222
feschmidt <felix.e.schmidt@gmail.com>
2020-05-29 11:41:49 +0200

    small updates to drivers

--- devices/RS_SMB100A.py
+++ devices/RS_SMB100A.py
@@ -14,7 +14,7 @@ def numtostr(mystr):
 
 class RS_SMB100A(instrument):
     def __init__(self,
-                 addr='TCPIP::192.168.1.216::INSTR',
+                 addr='TCPIP::192.168.1.103::INSTR',
                  reset=True,
                  verb=True):
         super().__init__(addr, reset, verb)
