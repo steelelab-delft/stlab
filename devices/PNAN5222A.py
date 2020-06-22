d8f58b5662c94b61dedcba48472d8bfa39005241
Hadi <hadi.arjmandi@gmail.com>
2020-06-22 16:29:22 +0200

    changes

--- devices/PNAN5222A.py
+++ devices/PNAN5222A.py
@@ -353,6 +353,16 @@ class PNAN5222A(PNAN5221A):
         self.write('DISP:WIND:TRAC2:MOVE 2')
         self.write('DISP:WIND:TRAC3:MOVE 2')
 
+    def Average(self,N_averages=5):
+        self.write('SENS:AVER:COUN %d'%N_averages)
+        self.write('SENS:AVER ON')
+        self.write('SENS:AVER:CLEAR')
+        naver = int(self.query('SENS:AVER:COUN?'))
+        for j in range(naver-1):
+            self.Trigger()
+        self.write('SENS:AVER OFF')
+
+
     #Not currently working for segments
     '''
     def SetSegmIFBW(self,x):
