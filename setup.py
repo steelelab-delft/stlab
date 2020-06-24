5c0437fc25b34eff959e6fa3454714be043b7624
Mario Gely - TNW <mfgely@tudelft.net>
2020-06-24 10:53:56 +0200

    setup.py working

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ import setuptools
 with open('requirements.txt') as f:
     reqs = f.readlines()
 
+# Remove links to github repos
+# these have to be added manually
+reqs = [ r for r in reqs if "github" not in r ]
+
 setuptools.setup(
     name="stlab",
     package_dir={'stlab': 'src'},
     packages= ['stlab'],
-    install_requires = reqs
+    install_requires = reqs+['stlabutils'],
+    dependency_links=['git+git://github.com/steelelab-delft/stlabutils.git']
 )
