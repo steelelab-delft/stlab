0d8bea48a81b7ae0d664fc88433da236f3ac4795
Mario Gely - TNW <mfgely@tudelft.net>
2020-06-24 13:17:36 +0200

    Now actually copying all the files when pip installing

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 import setuptools
+import os
+
 with open('requirements.txt') as f:
     reqs = f.readlines()
+
+    
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+package_data = package_files('src')
 
 setuptools.setup(
     name="stlab",
     package_dir={'stlab': 'src'},
     packages= ['stlab'],
     install_requires = reqs,
+    include_package_data=True,
+    package_data={'stlab': package_data},
 )
