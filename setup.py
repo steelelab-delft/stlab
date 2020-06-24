9e565f15f9421edd19a62723c8098fb11a5f5321
Mario Gely - TNW <mfgely@tudelft.net>
2020-06-24 11:11:28 +0200

    Test pip installation

--- setup.py
+++ setup.py
@@ -2,14 +2,9 @@ import setuptools
 with open('requirements.txt') as f:
     reqs = f.readlines()
 
-# # Remove links to github repos
-# # these have to be added manually
-# reqs = [ r for r in reqs if "github" not in r ]
-
 setuptools.setup(
     name="stlab",
     package_dir={'stlab': 'src'},
     packages= ['stlab'],
     install_requires = reqs,
-    # dependency_links=['git+git://github.com/steelelab-delft/stlabutils/tarball/master']
 )
