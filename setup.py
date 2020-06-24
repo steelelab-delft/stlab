5cba099203bf49d66ad2b581245a5fec2b74f83a
Mario Gely - TNW <mfgely@tudelft.net>
2020-06-24 10:41:28 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+with open('requirements.txt') as f:
+    reqs = f.readlines()
+
+setuptools.setup(
+    name="stlab",
+    package_dir={'stlab': 'src'},
+    packages= ['stlab'],
+    install_requires = reqs
+)
